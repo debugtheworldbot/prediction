ae02acb6917680d78bf10934c63b210490cd530d
tian <istiancz@gmail.com>
2024-04-18 20:38:20 +0800

    feat: auth google

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,2 @@
+import { signIn, signOut, auth, handlers } from "@/auth"; // Referring to the auth.ts we just created
+export const { GET, POST } = handlers;
--- app/prediction/[id]/page.tsx
+++ app/prediction/[id]/page.tsx
@@ -1,3 +1,23 @@
-export default function Detail({ params }: { params: { id: string } }) {
-  return <p>Post: {params.id}</p>;
+import { auth, signIn } from "@/auth";
+
+export function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signIn("google");
+      }}
+    >
+      <button type="submit">Signin with Google</button>
+    </form>
+  );
+}
+export default async function Detail({ params }: { params: { id: string } }) {
+  const session = await auth();
+  return (
+    <div>
+      <p>Post: {params.id}</p>
+      <SignIn />
+    </div>
+  );
 }
